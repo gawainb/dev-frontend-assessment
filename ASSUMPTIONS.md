524d3051710177f9107f2c35751e0addc6201bed
Gawain Bracy II <bracy.gawain2014@gmail.com>
2019-06-21 18:56:41 -0400

    Completed the following
    - Clean up and display the list of symbols in a UI/UX friendly way
    
    - Leverage any of the available data points currently being returned by the API to achieve this
    - Allow performing these actions on the list (do NOT re-fetch the data from the API):
    
    - Searching : On symbol and company name
    - Sorting : Ascending and descending on at least 1 data point
    - Filtering : On at least the following fields :
    "open"
    "close"
    "primaryExchange"
    
    - Allow excluding symbols by storing them in local storage and then preventing them from displaying in your main lists component
    
    - Add a menu item for viewing excluded symbols
    
    Signed-off-by: Gawain Bracy II <bracy.gawain2014@gmail.com>

---
+++ ASSUMPTIONS.md
@@ -0,0 +1,3 @@
+While implementing the filtering portion of the assesment I noticed there was not a "Primary Exchange" property in the data model. Because of this I did not know how to move forward in implementing it in my filtering.
+
+I also took advantage of the dropdown element within the "elements" folder that was already within the application. I noticed that it doesnt display an input box for selections.
--- src/views/Symbols.vue
+++ src/views/Symbols.vue
@@ -1,55 +1,180 @@
 <template>
-    <div>
-      <div class="primary-heading-con">
-          <div class="heading">
-              <div class="title">Symbols/Tickers</div>
-          </div>
+  <div>
+    <div class="primary-heading-con">
+      <div class="heading">
+        <div class="title">Symbols/Tickers</div>
       </div>
-
-      <div class="page-content-con">
-          <loading v-if="loading"></loading>
-          <div v-else>
-              <div
-                  v-for="company in companies"
-                  :key="company.symbol"
-                  class="company"
-              >
-                  <h5 class="heading is-size-5">{{company.symbol}} : <small class="is-size-7">{{company.companyName}}</small></h5>
-                  <div>Open <money :value="company.open"></money></div>
-                  <div>Close <money :value="company.close"></money></div>
-                  <timestamp :value="company.openTime"></timestamp> - <timestamp :value="company.closeTime"></timestamp>
-              </div>
-          </div>
-      </div>
-
     </div>
+
+    <div class="page-content-con">
+      <loading v-if="loading"></loading>
+      <div v-else>
+        <section class="section">
+          <div class="columns">
+            <div class="column">
+              <h5 class="is-size-5">Search</h5>
+              <input
+                class="input"
+                type="text"
+                placeholder="Search Ticker or Name"
+                v-model="textSearch"
+              >
+            </div>
+            <div class="column">
+              <h5 class="is-size-5">Sort</h5>
+              <dropdown-selection :selections="sortOptions" v-on:input="sortCompaniesByName"></dropdown-selection>
+            </div>
+            <div class="column">
+              <h5 class="is-size-5">Filter</h5>
+              <dropdown-selection :selections="options" v-on:input="filterCompanies"></dropdown-selection>
+            </div>
+            <div class="column">
+              <a class="button removeSymbolsBtn" v-on:click="isHidden = !isHidden">Remove Symbols</a>
+            </div>
+          </div>
+        </section>
+        <div class="coulumns">
+          <div class="column" v-for="company in filteredCompanies" :key="company.symbol">
+            <div class="company card">
+              <header class="card-header">
+                <a href="#">
+                  <h5 class="card-header-title is-size-5">
+                    <span v-if="!isHidden">{{company.symbol}} :&nbsp;</span>
+                    <small class="is-size-6 companyName">{{company.companyName}}</small>
+                  </h5>
+                </a>
+              </header>
+              <div class="card-content">
+                <div class="content">
+                  <div>
+                    Open
+                    <money :value="company.open"></money>
+                  </div>
+                  <div>
+                    Close
+                    <money :value="company.close"></money>
+                  </div>
+                  <timestamp :value="company.openTime"></timestamp>-
+                  <timestamp :value="company.closeTime"></timestamp>
+                </div>
+              </div>
+              <footer class="card-footer">
+                <a href="#" class="card-footer-item">Save</a>
+                <a href="#" class="card-footer-item">Edit</a>
+                <a href="#" class="card-footer-item">Delete</a>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import API from '../api/IEX';
+import API from "../api/IEX";
+import DropdownSelectionVue from "../elements/DropdownSelection.vue";
 export default {
-    name : "Symbols",
-    data () {
-        return {
-            loading : true,
-            companies : [],
-        };
+  name: "Symbols",
+  data() {
+    return {
+      loading: true,
+      isHidden: false,
+      textSearch: "",
+      sort: "",
+      options: [
+        { name: "Open", id: "open" },
+        { name: "Close", id: "close" },
+        { name: "Primary Exchange", id: "primaryExchange" }
+      ],
+      sortOptions: [{ name: "ASC", id: "asc" }, { name: "DESC", id: "desc" }],
+      companies: []
+    };
+  },
+  beforeMount() {
+    API.getComputerHardwareCompanies()
+      .then(response => {
+        this.companies = response.data;
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  },
+  computed: {
+    filteredCompanies: function() {
+      const textSearch = this.textSearch;
+      if (textSearch === "") {
+        return this.companies;
+      }
+      return this.companies.filter(function(el) {
+        return (
+          el.companyName.toLowerCase().indexOf(textSearch.toLowerCase()) !==
+            -1 ||
+          el.symbol.toLowerCase().indexOf(textSearch.toLowerCase()) !== -1
+        );
+      });
+    }
+  },
+  methods: {
+    filterCompanies: function(el) {
+      const companies = this.companies;
+      switch (el.id) {
+        case "open":
+          companies.sort(function(a, b) {
+            if (a.open === b.open) {
+              return 0;
+            }
+            return a.open < b.open ? -1 : 1;
+          });
+          break;
+        case "close":
+          companies.sort(function(a, b) {
+            if (a.close === b.close) {
+              return 0;
+            }
+            return a.close < b.close ? -1 : 1;
+          });
+          break;
+        case "primaryExchange":
+          companies.sort(function(a, b) {
+            if (a.companyName === b.companyName) {
+              return 0;
+            }
+            return a.companyName < b.companyName ? -1 : 1;
+          });
+          break;
+      }
     },
-    beforeMount () {
-        API.getComputerHardwareCompanies().then(response => {
-            this.companies = response.data;
-        }).finally(() => {
-            this.loading = false;
-        });
-    },
-}
+    sortCompaniesByName(el) {
+      const companies = this.companies;
+      switch (el.id) {
+        case "asc":
+          companies.sort(function(a, b) {
+            if (a.companyName === b.companyName) {
+              return 0;
+            }
+            return a.companyName < b.companyName ? -1 : 1;
+          });
+          break;
+        case "desc":
+          companies.sort(function(a, b) {
+            if (a.companyName === b.companyName) {
+              return 0;
+            }
+            return a.companyName > b.companyName ? -1 : 1;
+          });
+          break;
+      }
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-    @import '../assets/css/_theme';
-    .company {
-        margin-bottom:10px;
-        padding-bottom:10px;
-        border-bottom:1px solid $white-ter;
-    }
+@import "../assets/css/_theme";
+.company {
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid $white-ter;
+}
 </style>
